a439e3fd01aa51a78a8443ee7ae71320eaa125a9
9janinja <awure_barry@yahoo.com>
2025-01-12 23:24:29 +0100

    setup user profiles

--- bookclub/users/urls.py
+++ bookclub/users/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('register/', views.register, name='register'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
 ]
--- bookclub/users/views.py
+++ bookclub/users/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from .forms import RegisterForm
+from .models import Profile 
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
@@ -20,3 +22,24 @@ def register(request):
 @login_required
 def dashboard(request):
     return render(request, 'users/dashboard.html')
+
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        bio = request.POST['bio']
+        favorite_genre = request.POST['favorite_genre']
+        request.user.profile.bio = bio
+        request.user.profile.favorite_genre = favorite_genre    
+        request.user.profile.save()
+        return redirect('dashboard')    
+    return render(request, 'users/edit_profile.html', {'profile': request.user.profile})       
+     
+"""     profile = Profile.objects.get(user=request.user)
+    if request.method == 'POST':
+        form = RegisterForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard')
+    else:
+        form = RegisterForm(instance=profile)
+    return render(request, 'users/edit_profile.html', {'form': form}) """
