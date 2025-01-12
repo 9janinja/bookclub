5f04b9343a078d698ab7f49c67a8c43367800bc1
9janinja <awure_barry@yahoo.com>
2025-01-12 21:56:57 +0100

    setup user-registeration

--- bookclub/users/views.py
+++ bookclub/users/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import RegisterForm
 
 # Create your views here.
 def home(request):
     return HttpResponse("Hello, BookClub!")
 
-
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    return render(request, 'users/register.html', {'form': form})   
